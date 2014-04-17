c53ce5df389f6ab29a76248aa6564b735d320a09
moonlovestar <wayne80102@gmail.com>
2014-04-17 21:13:23 +0800

    Test for recursive and theoretical works.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,52 @@
 #include "InfoCaculator.h"
 #include <iostream>
 #include <vector>
+#include <string.h>
 using namespace std;
+void generTestAttr(vector<vector<string> >&attr)
+{
+	vector<string> row;
+	row.push_back("A1");
+	row.push_back("A2");
+	row.push_back("A3");
+	attr.push_back(row);
+	row.clear();
+	row.push_back("B1");
+	row.push_back("B2");
+	attr.push_back(row);
+	row.clear();
+	row.push_back("C1");
+	row.push_back("C2");
+	attr.push_back(row);
+	row.clear();
+
+}
+void recursiveTree(vector<string> cur,vector<string> thisTimeGain,vector<vector<string> > &attr,int time)
+{
+	time++;
+	//if end of distribute.
+	if(cur.size()==3)
+	{
+		cout<<"Result:"<<endl;
+		for(int i=0;i<cur.size();i++)
+		{
+			cout<<cur[i]<<" ";
+		}
+		cout<<endl;
+	}
+	else
+	{
+		//Calculate Gain
+		vector<string> nextTimeGain=attr[time];
+		for(int i=0;i<thisTimeGain.size();i++)
+		{
+			// cout<<thisTimeGain[i]<<" ";
+			cur.push_back(thisTimeGain[i]);
+			recursiveTree(cur,nextTimeGain,attr,time);
+			cur.pop_back();
+		}
+	}
+}
 int main(int argc, char **argv){
 	
 	// int inputA=9;
@@ -23,5 +68,10 @@ int main(int argc, char **argv){
 	attrVec.push_back(attrRow);
 	attrRow.clear();
 	cout<<"Slide example Info_age: "<<InfoCaculator::infoClassCal(3,14,attrVec)<<endl;
+
+	vector<vector<string> > attr;
+	generTestAttr(attr);
+	vector<string> result;
+	recursiveTree(result,attr[0],attr,0);
 	return 0;
 }
