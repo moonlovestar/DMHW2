66337220364c023a28339280e02b06e67f9df32c
ericwalker <ericdavid78929@gmail.com>
2014-04-20 19:48:07 +0800

    modify test.cpp

--- test.cpp
+++ test.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <fstream>
+#include <stdlib.h>
+#include <vector>
+#include <sstream>
 using namespace std;
 
 vector<string> splitStringToWords(string inputString,string delim)
@@ -25,6 +28,7 @@ vector<string> splitStringToWords(string inputString,string delim)
 struct People{
         float age;
         std::string workclass;
+        float fnlwgt;
         std::string education;
         float educationNum;
         std::string maritalStatus;
@@ -35,64 +39,74 @@ struct People{
         float capitalGain;
         float capitalLoss;
         float hoursPerWeek;
-        float country;
+        std::string country;
         std::string group;
 
 };
 
 int main()
 {
-	People* s[100];
     ifstream input;
-    ofstream output; 
+    ofstream output;
+
+    fstream file;
+    vector<People> people;
+    vector<string> tempString;
+    People* tempPeople;
+    tempPeople = new People;
+
     file.open("census1997.dat");
     if(file)
 	{
 	    string tempStr="";
 		while(getline(file,tempStr))
 		{
-			//  cout<<tempStr<<endl;
-			if(tempStr.find("spam")==0)
-			{
-				SpamSMS.push_back(tempStr);
-			//	cout<<"------------------------------"<<endl;
-			//	cout<<"This Message is Spam SMS"<<endl;
-			}
-			else
-			{
-			//	cout<<"------------------------------"<<endl;
-			//	cout<<"This Message is normal SMS"<<endl;
-				SMS.push_back(tempStr);
-			}
-
+			tempString = splitStringToWords(tempStr, " ,");
+			tempPeople->age = stof(tempString[0]);
+			tempPeople->workclass = tempString[1];
+			tempPeople->fnlwgt = stof(tempString[2]);
+			tempPeople->education = tempString[3];
+			tempPeople->educationNum = stof(tempString[4]);
+			tempPeople->maritalStatus = tempString[5];
+			tempPeople->occupation = tempString[6];
+			tempPeople->relationship = tempString[7];
+			tempPeople->race = tempString[8];
+			tempPeople->sex = tempString[9];
+			tempPeople->capitalGain = stof(tempString[10]);
+			tempPeople->capitalLoss = stof(tempString[11]);
+			tempPeople->hoursPerWeek = stof(tempString[12]);
+			tempPeople->country = tempString[13];
+			tempPeople->group = tempString[14];
+			people.push_back(tempPeople);
 		}
-
-
 	}
 	else
 	{
 		cout<<"Cannot open file!"<<endl;
 	}
 
+	//People* s[People.size()];
 
-    for(int i=0; i< 10; i++){
-                s[i] = new People; 
-                //vector<string> wordVector;
-				//wordVector=splitStringToWords(,",");
-				for(int j=0;j<wordVector.size();j++)
-				{
-					input >> s[i]->age >> s[i]->workclass >> s[i]->education >> s[i]->educationNum >> s[i]->maritalStatus >> s[i]->occupation
-					      >> s[i]->relationship >> s[i]->race >> s[i]->sex >> s[i]->capitalGain >> s[i]->capitalLoss >> s[i]->hoursPerWeek
-					       >> s[i]->country >> s[i]->group;
-				}
-                
-        }
-        cout << s[0]->age << endl;
-        cout << s[0]->workclass << endl;
-        cout << s[0]->education << endl;
-        cout << s[0]->educationNum << endl;
-        cout << s[0]->maritalStatus << endl;
-    system("pause");
+	/*for(int i=0;i<People.size();i++)
+	{
+		//vector<string> s;
+		vector<People> s;
+		s[i] = new People;
+		s=splitStringToWords(People,",");
+
+
+		//input >> s[i]->age >> s[i]->workclass >> s[i]->education >> s[i]->educationNum >> s[i]->maritalStatus >> s[i]->occupation
+      //>> s[i]->relationship >> s[i]->race >> s[i]->sex >> s[i]->capitalGain >> s[i]->capitalLoss >> s[i]->hoursPerWeek
+       //>> s[i]->country >> s[i]->group;		
+	}*/
+
+
+        // cout << s[0]->age << endl;
+        // cout << s[0]->workclass << endl;
+        // cout << s[0]->education << endl;
+        // cout << s[0]->educationNum << endl;
+        // cout << s[0]->maritalStatus << endl;
+    //system("pause");
     return 0;
 
 }
