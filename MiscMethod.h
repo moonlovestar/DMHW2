722d8c983957665d5602b4627af7e1d3af4c86f7
moonlovestar <wayne80102@gmail.com>
2014-04-22 23:34:02 +0800

    Seperate MiscMethod to a class.

--- MiscMethod.h
+++ MiscMethod.h
@@ -1,14 +1,12 @@
 #ifndef MISCMETHOD_H
 #define MISCMETHOD_H
 #include <iostream>
-#include <sstream>
-float stringToFloat(std::string str)
-{
-	std::stringstream ss;
-	float result;
-	ss<<str;	
-	ss>>result;
-	//std::cout<<result+10.3<<std::endl;
-	return result;
-}
+#include <vector>
+#include <string>
+class MiscMethod{
+	public:
+		static float stringToFloat(std::string str);
+		static std::vector<std::string> splitStringToAttrs(std::string inputString,std::string delim);
+};
+
 #endif
